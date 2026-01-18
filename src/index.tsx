a4da07399182b33cf58310a4fd2274d010709f51
Jason O'Neill <CreativeTechGuy@users.noreply.github.com>
2026-01-18 02:34:40 -0800

    Fix favicon

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,7 @@ import "./styles/index.css";
 import "./styles/classes.css";
 import "./styles/table.css";
 import { render } from "solid-js/web";
-import faviconIcon from "~/raw_data/icons/T_Hedgehog_icon_normal.png";
 import { App } from "./components/App";
 import { rootElement } from "./config/rootElement";
 
 render(() => <App />, rootElement);
-
-const faviconElem = document.createElement("link");
-faviconElem.rel = "icon";
-faviconElem.type = "image/png";
-faviconElem.href = faviconIcon;
-document.head.appendChild(faviconElem);
